06429d431de044447448d3ffb0fac74b6a1887c0
myngoc <myngocjdt@gmail.com>
2026-05-03 03:55:57 +0700

    Fix: load data hoadon, nhanvien, tinh tong tien chiet khau, sua designer nhanvien

--- RestaurantManagement/DatabaseHelper.cs
+++ RestaurantManagement/DatabaseHelper.cs
@@ -3,9 +3,7 @@ using Microsoft.Data.SqlClient;
 public class DatabaseHelper
 {
     private static readonly string ConnectionString =
-        "Server=LAPTOP-CP90VKG7;" +
-        "Database=QuanLyCuaHang;" +
-        "Integrated Security=True;" +
+        "Server=DESKTOP-JF58Q6V\\SQL2025;Database=QuanLyCuaHang;Integrated Security=True;" +
         "TrustServerCertificate=True;";
 
     public static SqlConnection GetConnection()
